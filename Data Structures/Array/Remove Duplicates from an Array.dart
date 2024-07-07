49447765483efd078206857a022ffad7202038ff
yasin <yasinmiapolash@gmail.com>
2024-07-07 21:57:52 +0600

    another way

--- Data Structures/Array/Remove Duplicates from an Array.dart
+++ Data Structures/Array/Remove Duplicates from an Array.dart
@@ -3,9 +3,22 @@
 void main() {
   List<int> arr = [1, 2, 9, 4, 4, 5, 6, 6, 7, 2];
   print(checkDuplicates(arr));
+  print(checkValue(arr));
 }
 
 List<int> checkDuplicates(List<int> arr) {
   Set<int> convertToSet = arr.toSet();
   return convertToSet.toList();
+}
+
+List<int> checkValue(List<int> arr) {
+  Set<int> seen = Set();
+  List<int> uniqueValue = [];
+  for (int i in arr) {
+    if (!seen.contains(i)) {
+      seen.add(i);
+      uniqueValue.add(i);
+    }
+  }
+  return uniqueValue;
 }
