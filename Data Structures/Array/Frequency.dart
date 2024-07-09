e1b45835d7717034afe35b828c8f03bbbdfa263b
yasin <yasinmiapolash@gmail.com>
2024-07-09 22:54:47 +0600

    array done

--- Data Structures/Array/Frequency.dart
+++ Data Structures/Array/Frequency.dart
@@ -1,2 +1,18 @@
 // Problem: Given an array of integers, find the frequency of each element.
 
+Map<int, int> findFrequency(List<int> arr) {
+  Map<int, int> frequency = {};
+  for (int num in arr) {
+    if (frequency.containsKey(num)) {
+      frequency[num] = frequency[num]! + 1;
+    } else {
+      frequency[num] = 1;
+    }
+  }
+  return frequency;
+}
+
+void main() {
+  List<int> arr = [1, 2, 2, 3, 4, 4, 6, 6, 7];
+  print(findFrequency(arr));
+}
