5f63f4b8ccc68812cf45a0d69751d8770713ccd2
yasin <yasinmiapolash@gmail.com>
2025-02-01 23:55:06 +0600

    style: format code for consistency in Grid_Counter algorithm

--- Algorithms/quadratic time complexity(O(n2))/Grid_Counter.c
+++ Algorithms/quadratic time complexity(O(n2))/Grid_Counter.c
@@ -1,16 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int i,j,n,count;
-    scanf("%d",&n);
-    count=0;
-    for(i=0;i<n;i++)
+    int i, j, n, count;
+    scanf("%d", &n);
+    count = 0;
+    for (i = 0; i < n; i++)
     {
-        for(j=0;j<n;j++)
+        for (j = 0; j < n; j++)
         {
-            count=count+1;
+            count = count + 1;
         }
-       
     }
     printf("n=%d,count=%d\n", n, count);
 
