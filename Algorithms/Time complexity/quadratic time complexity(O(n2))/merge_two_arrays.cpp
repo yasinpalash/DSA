37d07335d799abcf58031c1201cdd0b2367fc975
yasin <yasinmiapolash@gmail.com>
2025-06-21 23:34:15 +0600

    array of marge

---
+++ Algorithms/Time complexity/quadratic time complexity(O(n2))/merge_two_arrays.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    int a[n], b[m];
+    for (int i = 0; i < n; i++)
+        cin >> a[i];
+    for (int i = 0; i < m; i++)
+        cin >> b[i];
+    int c[n + m];
+    for (int i = 0; i < n + m; i++)
+    {
+
+        if (i < n)
+            c[i] = a[i];
+        else
+            c[i] = b[i - n];
+    }
+
+    for (int i = 0; i < n + m; i++)
+        cout << c[i] << "";
+    return 0;
+}
